8ea9f9cdf348b2a26f2aabafdbdabd8f8c270812
Stefan Kantardzhiev <stefan.ivailov@gmail.com>
2023-07-06 16:07:09 +0200

    Nav done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,62 @@
 import React from 'react'
 // import { Container, Row } from 'reactstrap'
 import logo from '../../assets/images/eco-logo.png'
+import { Link, NavLink } from 'react-router-dom'
+const navLinks = [
+    {
+        path: '/',
+        display: 'Home'
+    },
+    {
+        path: 'shop',
+        display: 'Shop'
+    },
+    {
+        path: 'cart',
+        display: 'Cart'
+    }
+]
 
+const navLinksNewUser = [
+    {
+        path: 'login',
+        display: 'Login'
+    },
+    {
+        path: 'signup',
+        display: 'Sign Up'
+    },
+]
 
 const Header = () => {
+
     return (
         <header className='header'>
             <div className='nav_wrapper'>
-
                 <div className='logo'>
+                    <img src={logo} alt='logo' />
                     <div className='logo-text'>
-                        <img src={logo} alt='logo' />
-                        <h1>PCBuildz.de</h1>
-                        <p>From enthusiasts for enthusiasts!</p>
+                        <Link to={'/'}><h3>PCBuildz.de</h3>
+                        <p>From enthusiasts for enthusiasts!</p></Link>
                     </div>
                 </div>
-                <ul className='menu'>
-                    {/* if user */}<div className='logged'>
-                        <li className='nav_item'>Home</li>
-                        <li className='nav_item'>Shop</li>
-                        <li className='nav_item'>Cart</li></div>
+                {/* if user */}
+                <div className='logged'>
 
+                    {navLinks.map((item, index) => (
+                        <li className='nav_item' key={index}>
+                            <NavLink to={item.path} className={(navClass) => navClass.isActive ? "nav_active" : ''} >{item.display}</NavLink>
+                        </li>
+                    ))}
+                </div>
+                <div className="not-logged">
                     {/* if not user */}
-                    <div className="not-logged">
-                        <li className='nav_item'>Login</li>
-                        <li className='nav_item'>Sign Up</li>
-                    </div>
-                </ul>
+                    {navLinksNewUser.map((item, index) => (
+                        <li className='nav_item' key={index}>
+                            <NavLink to={item.path} className={(navClass) => navClass.isActive ? "nav_active" : ''} >{item.display}</NavLink>
+                        </li>
+                    ))}
+                </div>
 
             </div>
         </header>
